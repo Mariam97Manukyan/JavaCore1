897459028b31010eb2b99257ae147e884cd6bd8b
MARIAM <mariam97manukyan.97@gmail.com>
2023-01-24 18:36:49 +0400

    add homework 9

--- src/homeworks/homework8/DynamicArrayDemo.java
+++ src/homeworks/homework8/DynamicArrayDemo.java
@@ -3,7 +3,7 @@ package homeworks.homework8;
 public class DynamicArrayDemo {
     public static void main(String[] args) {
         DynamicArray dy = new DynamicArray();
-        dy.add(15);
+        dy.add(3);
         dy.add(26);
         dy.add(36);
         dy.add(41);
@@ -21,6 +21,27 @@ public class DynamicArrayDemo {
         System.out.println(dy.getByIndex(40));
         System.out.print("Array after adding  elements: ");
         dy.print();
+        dy.deleteByIndex(4);
+        dy.deleteByIndex(-1);
+        dy.deleteByIndex(28);
+        dy.print();
+        dy.add(0, 18);
+        dy.add(-1, 5);
+        dy.add(40, 5);
+        dy.print();
+        dy.set(1, 5);
+        dy.set(-1, 5);
+        dy.set(19, 5);
+        dy.print();
+        boolean result = dy.exists(66);
+        System.out.println(result);
+        result = dy.exists(100);
+        System.out.println(result);
+
+        int index = dy.getIndexByValue(26);
+        System.out.println("Index of 26: " + index);
+        index = dy.getIndexByValue(100);
+        System.out.println("Index of 100: " + index);
 
 
     }
